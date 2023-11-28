7fee2fdd8eeee09efb537c52f03f350114ff38f6
sachin_a <sachin_a@epiclanka.net>
2023-11-28 14:27:55 +0530

    Initial commit

---
+++ src/main/java/com/example/registrationlogindemo/dto/UserDto.java
@@ -0,0 +1,27 @@
+package com.example.registrationlogindemo.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto
+{
+    private Long id;
+    @NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+    @NotEmpty(message = "Password should not be empty")
+    private String password;
+}
